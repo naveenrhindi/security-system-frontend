8146259bdf270a316a38346cd3c01db2a9ffd0ae
naveenrhindi <naveenhindi4@gmail.com>
2025-07-05 18:46:05 +0530

    testing

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,107 +1,33 @@
-// import { createContext, useEffect, useState } from "react";
-// import { AppConstants } from "../util/constants";
-// import { toast } from "react-toastify";
-// import axios from "axios";
-// export const AppContext = createContext();
-
-// export const AppContextProvider = (props) => {
-
-//     axios.defaults.withCredentials = true;  
-
-//   const backendURL = AppConstants.BACKEND_URL;
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const [userData, setUserData] = useState(false);
-
-//   const getUserData = async () => {
-//     try {
-//       const response = await axios.get(backendURL + "/profile");
-//       if (response.status === 200) {
-//         setUserData(response.data);
-//       } else {
-//         toast.error("Unable to retrieve profile");
-//       }
-//     } catch (error) {
-//       toast.error(error.message);
-//     }
-//   };
-
-//   const getAuthState = async () => {
-//     try {
-//       const response = await axios.get(backendURL + "/is-authenticated");
-//       if (response.status === 200 && response.data === true) {
-//         setIsLoggedIn(true);
-//         await getUserData();
-//       } else {
-//         setIsLoggedIn(false);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       setIsLoggedIn(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     getAuthState();
-//   }, []);
-
-//   const contextValue = {
-//     backendURL,
-//     isLoggedIn,
-//     setIsLoggedIn,
-//     userData,
-//     setUserData,
-//     getUserData,
-//   };
-
-//   return (
-//     <AppContext.Provider value={contextValue}>
-//       {props.children}
-//     </AppContext.Provider>
-//   );
-// };
-
-
-
-
-
 import { createContext, useEffect, useState } from "react";
 import { AppConstants } from "../util/constants";
 import { toast } from "react-toastify";
 import axios from "axios";
-
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+
+    axios.defaults.withCredentials = true;  
+
   const backendURL = AppConstants.BACKEND_URL;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userData, setUserData] = useState(null);
-
-  // Attach token to requests
-  const getAuthHeaders = () => {
-    const token = localStorage.getItem("jwtToken");
-    return {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-  };
+  const [userData, setUserData] = useState(false);
 
   const getUserData = async () => {
     try {
-      const response = await axios.get(backendURL + "/profile", getAuthHeaders());
+      const response = await axios.get(backendURL + "/profile");
       if (response.status === 200) {
         setUserData(response.data);
       } else {
         toast.error("Unable to retrieve profile");
       }
     } catch (error) {
-      toast.error(error.message || "Error retrieving profile");
+      toast.error(error.message);
     }
   };
 
   const getAuthState = async () => {
     try {
-      const response = await axios.get(backendURL + "/is-authenticated", getAuthHeaders());
+      const response = await axios.get(backendURL + "/is-authenticated");
       if (response.status === 200 && response.data === true) {
         setIsLoggedIn(true);
         await getUserData();
@@ -109,7 +35,7 @@ export const AppContextProvider = (props) => {
         setIsLoggedIn(false);
       }
     } catch (error) {
-      console.error("Auth check failed:", error.message);
+      console.error(error);
       setIsLoggedIn(false);
     }
   };
@@ -133,3 +59,6 @@ export const AppContextProvider = (props) => {
     </AppContext.Provider>
   );
 };
+
+
+
